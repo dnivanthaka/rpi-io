7b8f078891892a7ae42ae6bb26c80816f5a41e16
Dinusha Nivanthaka Amerasinghe <dinusha@dinusha-HP-Pro-3130-Microtower-PC>
2017-05-03 16:16:57 +0530

    Minor modification to mpu6050 code

--- dev/mpu6050/main.cpp
+++ dev/mpu6050/main.cpp
@@ -12,14 +12,20 @@ using namespace rpiIO;
 int main(void)
 {
     uint8_t whoami;
+    int16_t temp;
 
     cout.flags(ios::right | ios::hex | ios::showbase);
     cout.width(4);
     
     I2CDevice mpu(I2C_1, MPU6050_ADDR);
     mpu.read8Register(&whoami, 0x75);
+    // Reading temperature
+    mpu.read16Register(&temp, 0x41);
+    temp = temp/340 + 36.53
 
     cout << (int)whoami << endl;
+    cout.unsetf(ios::right | ios::hex | ios::showbase);
+    cout << temp << endl;
 
     return 0;
 }
